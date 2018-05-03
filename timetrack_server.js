239e9bfdd63f6a3c22b40594cfe373d61e8edda2
zhiyong wang <36891314@163.com>
2018-05-04 05:27:09 +0800

    s

--- timetrack_server.js
+++ timetrack_server.js
@@ -4,9 +4,10 @@ var mysql = require('mysql');
 
 var db = mysql.createConnection({
     host: '127.0.0.1',
-    user: 'myuser',
-    password: 'mypassword',
-    database: 'timetrack'
+    user: 'root',
+    password: '12345678',
+    database: 'timetrack',
+    insecureAuth : true
 });
 
 
@@ -39,13 +40,13 @@ var server = http.createServer(function(req, res) {
 
 
 db.query(
-    "CREATE TABLE IF NOT EXISTS work("
+    "CREATE TABLE IF NOT EXISTS work ("
     + "id INT(10) NOT NULL AUTO_INCREMENT,"
     + "hours DECIMAL(5,2) DEFAULT 0,"
     + "date DATE, "
     + "archived INT(1) DEFAULT 0,"
     + "description LONGTEXT,"
-    + "PRIMARY KEY(id)",
+    + "PRIMARY KEY(id))",
     function (err) {
         if(err) throw err;
         console.log('Server started...')
